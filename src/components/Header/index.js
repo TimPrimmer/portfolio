0044de1409b0f5704a4095d07fb89088e9c6a916
Tim Primmer <timprimmer@gmail.com>
2021-09-23 00:40:54 -0500

    added page selection styling and more general changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,22 +15,67 @@ function Header(props) {
     document.title = tempString;
   }, [currentCategory]);
 
+  let aboutMeClicked;
+  let portfolioClicked;
+  let contactClicked;
+  let resumeClicked;
+
+  const highlightTab = (clickedTab) => {
+    console.log(aboutMeClicked);
+    switch (clickedTab.name) {
+      case "about me": {
+        aboutMeClicked = true;
+        break;
+      }
+      case "portfolio": {
+        portfolioClicked = true;
+        break;
+      }
+      case "contact": {
+        contactClicked = true;
+        break;
+      }
+      case "resume": {
+        resumeClicked = true;
+        break;
+      }
+      default: {
+        aboutMeClicked = true;
+        break;
+      }
+    }
+    console.log(clickedTab);
+  }
+
+  highlightTab(currentCategory);
+
   return (
     <header>
       <h1><a href="./">Tims Portfolio</a></h1>
       <nav>
         <ul id="main-nav">
           <li>
-            <h2><a href="#about-section" onClick={() => setCurrentCategory(categories[0])}>About Me</a></h2>
+            <h2>
+              <a href="#about-section" id="about-link" className={`${aboutMeClicked ? 'active-tab' : 'non-active-tab'}`} onClick={() => {
+                setCurrentCategory(categories[0])
+              }}>About Me
+              </a>
+            </h2>
           </li>
           <li>
-            <h2><a href="#portfolio-section" onClick={() => setCurrentCategory(categories[1])}>Portfolio</a></h2>
+            <h2><a href="#portfolio-section" id="portfolio-link" className={`${portfolioClicked ? 'active-tab' : 'non-active-tab'}`} onClick={() => {
+              setCurrentCategory(categories[1])
+            }}>Portfolio</a></h2>
           </li>
           <li>
-            <h2><a href="#contact-section" onClick={() => setCurrentCategory(categories[2])}>Contact</a></h2>
+            <h2><a href="#contact-section" id="contact-link" className={`${contactClicked ? 'active-tab' : 'non-active-tab'}`} onClick={() => {
+              setCurrentCategory(categories[2])
+            }}>Contact</a></h2>
           </li>
           <li>
-            <h2><a href="#resume-section" onClick={() => setCurrentCategory(categories[3])}>Resume</a></h2>
+            <h2><a href="#resume-section" id="resume-link" className={`${resumeClicked ? 'active-tab' : 'non-active-tab'}`} onClick={() => {
+              setCurrentCategory(categories[3])
+            }}>Resume</a></h2>
           </li>
         </ul>
       </nav>
